dffc7c12269f25e2fa0eafee14d0021f8cf7cf04
Alex Inkin <ANInkin@mail.ru>
2016-04-03 00:19:27 +0400

    5

--- www/index.php
+++ www/index.php
@@ -2,14 +2,14 @@
 require_once("inc/config.php");
 require_once("inc/db_func.php");
 
-//if(isset($_SESSION["idUser"]))
-	//{
-	//require_once($LOCAL_PATH."/maket/framework.php");
-	//}
-	//else
-	//{
+if(isset($_SESSION["idUser"]))
+	{
+	require_once($LOCAL_PATH."/maket/framework.php");
+	}
+	else
+	{
 	require_once($LOCAL_PATH."/maket/login.php");
-	//}
+	}
 ?>
 
 
--- www/maket/login.php
+++ www/maket/login.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
     <link rel="shortcut icon" href="/images/favicon.ico">
 
     <title>Just CHAT</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
     <link href="assets/css/signin.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
@@ -31,13 +31,11 @@
     <div class="container">
 
       <form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <h2 class="form-signin-heading">Войдите пожалуйста!</h2>
+        <input type="login" class="form-control" placeholder="Логин" required autofocus>
+        <input type="password" class="form-control" placeholder="Пароль" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Войти!</button>
+        <a href="#regModal" role="button" class="btn btn-lg btn-primary btn-block" data-toggle="modal" data-target="#regModal">Зарегистрироваться!</a>
       </form>
 
     </div> <!-- /container -->
@@ -46,5 +44,46 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+    <div class="modal fade" id="regModal" tabindex="-1" role="dialog" aria-labelledby="regModalLabel" aria-hidden="true" style="z-index:2001;">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="form-signin-heading" id="myModalLabel">Регистрация нового пользователя</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="centered">
+                        <div id="cont"></div>
+                        <div class="input-field col-lg-12">
+                            <i class="fa fa-user prefix"></i>
+                            <input name="login" id="login" type="login" placeholder="Логин" class="form-control" autofocus required>
+                        </div>
+                        <div class="input-field col-lg-12">
+                            <i class="fa fa-key prefix"></i>
+                            <input name="pass" id="pass" type="password" placeholder="Пароль" class="form-control" required>
+                        </div>  
+                        <div class="input-field col-lg-12">
+                            <i class="fa fa-key prefix"></i>
+                            <input name="pass_confirm" id="pass_confirm" type="password" placeholder="Подтверждение пароля" class="form-control" required>
+                        </div> 
+                        <div class="input-field col-lg-12">
+                            <i class="fa fa-text-width prefix"></i>
+                            <input name="fio" id="fio" type="text" placeholder="ФИО" class="form-control" required>
+                        </div>  
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="centered">
+                        <button id="done_reg" type="submit" class="btn btn-lg btn-primary btn-block">ЗАРЕГИСТРИРОВАТЬСЯ</button>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
   </body>
 </html>
+<script type="text/javascript">
+        $("document").ready(function(){
+            $('#modal').modal();
+        });
+    </script>
